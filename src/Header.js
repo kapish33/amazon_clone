1693ed4833f5e91e93e39656825a98a7147c3fc8
Kapish Singh <kapishsingh2209@gmail.com>
2021-10-23 22:23:29 +0530

    Header Cloned!

---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="Amazon"
+      ></img>
+      <div className="header__search">
+        <input className="header__searchInput" type="text"></input>
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header_nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Return</span>
+          <span className="header__optionLineTwo">Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAlywCXe8wdIX68AFJ9UN5P10UDu7QLxk0",
+  authDomain: "challenge-a6cfe.firebaseapp.com",
+  projectId: "challenge-a6cfe",
+  storageBucket: "challenge-a6cfe.appspot.com",
+  messagingSenderId: "411922812062",
+  appId: "1:411922812062:web:d1879a782ab4f3e9bf7f8f",
+  measurementId: "G-LP0QFDGFZ4",
+};
