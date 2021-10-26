59b62b13c588745a7befd71c3a7514d0235a63d1
Kapish Singh <kapishsingh2209@gmail.com>
2021-10-26 14:41:30 +0530

    css issue : added signup and css signup :firebase:

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+  const regester = (e) => {
+    e.preventDefault();
+    //back to fiirebase
+  };
   return (
     <div className="login">
       <Link to="/">
@@ -16,19 +25,37 @@ function Login() {
         <h1>Sign-in</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button className="login__signInButton">Sign In</button>
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
         </form>
         <p>
           By signng-in you agree to Amazon's FAKe Clone Condition of use & Sale.
           Please see our Privacy Notice, our Cookies Notice and our
           Intrest-BAsed Ads Notice
         </p>
-        <button className="login__regesterButton">
+        <button
+          type="submit"
+          onClick={regester}
+          className="login__regesterButton"
+        >
           Create an Amazon account
         </button>
       </div>
